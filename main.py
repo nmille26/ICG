64b259e72b21d6477320dab5670a981f53c9af10
Nicola Miller <nmille26@binghamton.edu>
2022-11-17 15:51:21 -0500

    My project first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from dance import addnumbers, Leute
+
+
+def naming():
+    pants = Leute("Nic")
+    print(pants)
+
+
+if __name__ == '__main__':
+    print("pycharm is awesome")
+    nimble = "Ahmed"
+    print("Ahmed")
+    #     TODO: Learn Pycharm
+    naming()
+print(addnumbers(2, 3))
